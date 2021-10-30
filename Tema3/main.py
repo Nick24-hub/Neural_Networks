159b043feb13954899ebd27ce0501919f934f9be
Alexandrescu Nicolae <nicu24dec.an@gmail.com>
2021-10-30 18:38:37 +0300

    update some lines

--- Tema3/main.py
+++ Tema3/main.py
@@ -8,6 +8,8 @@ input_layer_size = 784
 hidden_layer_size = 100
 output_layer_size = 10
 learning_rate = 0.03
+regularization = 0
+momentum = 0
 
 with gzip.open('mnist.pkl.gz', 'rb') as fd:
     train_set, valid_set, test_set = pickle.load(fd, encoding='latin')
@@ -24,6 +26,10 @@ test_in = np.asarray(test_set[0])
 test_in = test_in.reshape((len(test_in), input_layer_size, 1))
 test_label = np.asarray(test_set[1])
 
+valid_in = np.asarray(valid_set[0])
+valid_in = valid_in.reshape((len(valid_in), input_layer_size, 1))
+valid_label = np.asarray(valid_set[1])
+
 xl1 = np.zeros((input_layer_size, 1))
 xl2 = np.zeros((hidden_layer_size, 1))
 xl3 = np.zeros((output_layer_size, 1))
@@ -34,8 +40,11 @@ wl2 = np.random.normal(0, 1 / np.sqrt(hidden_layer_size), (output_layer_size, hi
 W = [wl1, wl2]
 
 bl1 = np.random.normal(0, 1 / np.sqrt(input_layer_size), (hidden_layer_size, 1))
-bl2 = np.random.normal(0, 1 / np.sqrt(input_layer_size), (output_layer_size, 1))
+bl2 = np.random.normal(0, 1 / np.sqrt(hidden_layer_size), (output_layer_size, 1))
 B = [bl1, bl2]
+
+vl1 = np.zeros((hidden_layer_size, input_layer_size))
+vl2 = np.zeros((output_layer_size, hidden_layer_size))
 
 
 def sigmoid(x):
@@ -66,18 +75,20 @@ for epoch in range(epochs):
             y = softmax(z)
             xl3 = np.copy(y)
             t = np.zeros((output_layer_size, 1))
-            t[x_label[index]] = 1
-            error_l3 = (xl3 - t)
+            t[x_label[index]][0] = 1
+            error_l3 = xl3 - t
             deltal2 += np.dot(error_l3, xl2.T)
             betal2 += error_l3
             error_l2 = np.dot(np.dot(xl2, (1 - xl2).T), np.dot(wl2.T, error_l3))
             deltal1 += np.dot(error_l2, xl1.T)
             betal1 += error_l2
-        wl1 -= deltal1 * learning_rate / batch_size
+        vl1 = momentum * vl1 - deltal1 * learning_rate / batch_size
+        wl1 = (1 - regularization * learning_rate / batch_size) * wl1 + vl1
         bl1 -= betal1 * learning_rate / batch_size
-        wl2 -= deltal2 * learning_rate / batch_size
+        vl2 = momentum * vl2 - deltal2 * learning_rate / batch_size
+        wl2 = (1 - regularization * learning_rate / batch_size) * wl2 + vl2
         bl2 -= betal2 * learning_rate / batch_size
-    acuracy = 0
+    count = 0
     for example in range(len(test_in)):
         z = np.dot(wl1, test_in[example]) + bl1
         y = sigmoid(z)
@@ -85,6 +96,6 @@ for epoch in range(epochs):
         y = softmax(z)
         result = np.argmax(y)
         if result == test_label[example]:
-            acuracy = acuracy + 1
-    rate = acuracy * 100 / len(test_in)
-    print("Rate:", rate)
+            count = count + 1
+    acuracy = count * 100 / len(test_in)
+    print("Acuracy:", acuracy)
